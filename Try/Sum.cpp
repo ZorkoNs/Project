48418b9e27d990e0b4e26dc4b8d07b8a5af0f159
ZorkoN <46876026+ZorkoNs@users.noreply.github.com>
2019-01-22 02:30:31 +0300

    begin

--- Try/Sum.cpp
+++ Try/Sum.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 int main ()
 {
+	int i;
+	cout<<"Hello";
+	i=getch(); //ожидание отклика нажатие, но прерывает исполнение программы
+	cout<<"Key = "<<data();
 	
+	getch();
 	return 0;
 }
