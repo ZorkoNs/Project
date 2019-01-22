7ca7f0a638084b121e32af6e4f455b013353dd55
ZorkoN <46876026+ZorkoNs@users.noreply.github.com>
2019-01-22 16:54:04 +0300

    Update Sum.cpp

--- Try/Sum.cpp
+++ Try/Sum.cpp
@@ -6,12 +6,20 @@
 using namespace std;
 
 int main ()
-{ int a;
-	char i;
-	a=getch();
-	i=a;
-	cout<<"You press key = "<<a<<" is '"<<i<<"'";
-	cout<<"Hello Shaman";	
+{
+   int y;
+   cout<<“Leap year:»<<endl;
+   for (y=0; y<=2600; y++)
+   {
+      if (y%400=0) 
+         {
+         cout<<y;
+         }
+      else if (y%100=0) else if (y%4=0) {
+         cout<<y;
+         }
+   }
+   cout<<“That’s all!”;
 	getch();
 	return 0;
 }
