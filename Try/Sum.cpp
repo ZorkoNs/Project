e74ee09386dece623b9d7c3c7de2a89e2ab5fcb2
ZorkoN <46876026+ZorkoNs@users.noreply.github.com>
2019-01-24 00:30:58 +0300

    2

--- Try/Sum.cpp
+++ Try/Sum.cpp
@@ -6,20 +6,21 @@
 using namespace std;
 
 int main ()
-{
-   int y;
-   cout<<“Leap year:»<<endl;
+{  int y,a;
+	a=0;
+   cout<<"Leap year:"<<endl;
    for (y=0; y<=2600; y++)
    {
-      if (y%400=0) 
-         {
-         cout<<y;
-         }
-      else if (y%100=0) else if (y%4=0) {
-         cout<<y;
-         }
+      if (y%400==0) {a++;
+					cout<<y<<" ";
+				}
+      else if (y%100==0) {} else if (y%4==0) {a++;
+												cout<<y<<" ";
+												}
+      //~ if((y%100!=0&&y%4==0)||y%400==0)  {a++;  cout<<y<<" ";} //сокращённое написание
    }
-   cout<<“That’s all!”;
+   
+   cout<<"That's all! Iteration ="<<a;
 	getch();
 	return 0;
 }
